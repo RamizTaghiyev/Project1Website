fb347e694cd6c5ddf23f09e757c4401b0ae18c60
Ramiz Taghiyev <91952906+RamizTaghiyev@users.noreply.github.com>
2025-07-01 15:34:24 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const blocks = document.querySelectorAll('.lazy-block');
-
-    const observer = new IntersectionObserver((entries, obs) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
- 
-          obs.unobserve(entry.target);
-        }
-      });
-    }, {
-      threshold: 0.1   
-    });
-
-    blocks.forEach(block => observer.observe(block));
-  });
+document.querySelectorAll('.lazy-block').forEach(el => 
+  new IntersectionObserver(entries => 
+    entries.forEach(e => e.isIntersecting && e.target.classList.add('visible'))
+  ).observe(el)
+);
